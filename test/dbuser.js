4498c1e3c05a04976a50dca96234f217bbb66cd3
wangcheng <wangcheng714@gmail.com>
2013-09-02 11:14:42 +0800

    修改worker

--- test/dbuser.js
+++ test/dbuser.js
@@ -5,7 +5,7 @@ var fis = require("./../fis-cloud-kernel.js"),
 //    password = '890714',
 //    _auth = Base64.encode((name + password).toString());
 //
-//fis.db.insert('user', {}, {username : name, password : password, _auth : _auth}, {} , function(err, result){
+//fis.db.insert('user', {}, {_id : name, username : name, password : password, _auth : _auth}, {} , function(err, result){
 //    console.log(result);
 //});
 //
@@ -13,28 +13,28 @@ var fis = require("./../fis-cloud-kernel.js"),
 //password = '22222';
 //_auth = Base64.encode((name + password).toString());
 //
-//fis.db.insert('user', {}, {username : name, password : password, _auth : _auth}, {} , function(err, result){
+//fis.db.insert('user', {}, {_id : name, username : name, password : password, _auth : _auth}, {} , function(err, result){
 //    console.log(result);
 //});
 
-//fis.db.findOne("user", {}, {username:"wangcheng"}, function(err, result){
-//    if(!err){
-//        console.log("ok" + result);
-//    }else{
-//        console.log("error" + err);
-//    }
-//
-//});
-
-fis.db.find("user", "wangcheng", {}, {}, {}, function(err, result){
+fis.db.findOne("user", "wangcheng", {username:"wangcheng"}, function(err, result){
     if(!err){
-        result.toArray(function(err, users){
-            //console.log(users);
-            console.log(users[0]);
-            console.log(users[1]);
-        });
+        console.log(result);
     }else{
-        console.log(err);
+        console.log("error" + err);
     }
+
 });
+
+//fis.db.find("user", "", {}, {}, {}, function(err, result){
+//    if(!err){
+//        result.toArray(function(err, users){
+//            //console.log(users);
+//            console.log(users[0]);
+//            console.log(users[1]);
+//        });
+//    }else{
+//        console.log(err);
+//    }
+//});
 
